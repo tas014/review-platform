67ba454a1e553a7bf1aa9388342861e631c9d92d
tas014 <francoapicco@gmail.com>
2025-08-27 20:04:48 -0300

    Initializing Repo

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+
+const greetMsg = ref("");
+const name = ref("");
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsg.value = await invoke("greet", { name: name.value });
+}
+</script>
+
+<template>
+  <main class="container">
+    <h1>Hello World</h1>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: rgb(45, 45, 45);
+}
+</style>
---
+++ src/components/Modes.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <div class="mode-container">
+        <h1>Replay Mode</h1>
+      </div>
+      <div class="mode-container">
+        <h1>Analysis Mode</h1>
+      </div>
+    </div>
+  </header>
+</template>
+<style scoped>
+header {
+  height: 5vh;
+}
+.wrapper {
+  border-bottom-left-radius: 25%;
+  border-bottom-right-radius: 25%;
+  background-color: var(--dark-green);
+  padding: 1rem;
+}
+.mode-container {
+  display: grid;
+  place-content: center;
+}
+</style>
